e0fb083ab2ed58a32c817ca4af4cbbd9f4571235
Quentin Sc <Quentin Sc>
2017-04-22 22:48:26 +0200

    Ajoutez des fichiers projet.

---
+++ testGesture/testGesture/UserControl.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace testGesture
+{
+	public partial class UserControl : ContentView
+	{
+		public UserControl ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
